9524415ae31d1e82df6e26876af48cf9daf9657e
Knyazev Dmitry <noojeetso@gmail.com>
2023-02-01 22:38:51 +0300

    add unit tests macro functions && add unit test

--- unit_tests/check_determinization.c
+++ unit_tests/check_determinization.c
@@ -8,114 +8,69 @@
 #define POS_IN(x) "pos_" x "_in.txt"
 #define POS_OUT(x) "pos_" x "_out.txt"
 
-START_TEST(test_rm_lambda_3_verts_2_lambdas)
-{
-    int rc;
+#define TEST_HEAD(num)                                                                  \
+                            int rc;                                                     \
+                                                                                        \
+                            graph_t *graph;                                             \
+                            graph_t *new_graph;                                         \
+                            graph_t *exp_graph;                                         \
+                                                                                        \
+                            graph = graph_from_file(DATA_FOLDER POS_IN( num ));         \
+                            ck_assert_ptr_nonnull(graph);                               \
+                            exp_graph = graph_from_file(DATA_FOLDER POS_OUT( num ));    \
+                            ck_assert_ptr_nonnull(exp_graph);
 
-    graph_t *graph;
-    graph_t *new_graph;
-    graph_t *exp_graph;
+#define TEST_TAIL                                                                       \
+                            rc = compare_graphs(new_graph, exp_graph);                  \
+                            ck_assert_int_eq(rc, GRAPHS_EQUAL);                         \
+                                                                                        \
+                            free_graph(graph);                                          \
+                            free_graph(exp_graph);                                      \
+                            free_graph(new_graph);                                      \
 
-    graph = graph_from_file(DATA_FOLDER POS_IN("01"));
-    ck_assert_ptr_nonnull(graph);
+#define TEST(name, num)                                                                 \
+                        START_TEST( name )                                              \
+                        {                                                               \
+                            TEST_HEAD( num )                                            \
+                                                                                        \
+                            new_graph = remove_lambda_transitions(graph);               \
+                                                                                        \
+                            TEST_TAIL                                                   \
+                        }                                                               \
+                        END_TEST
 
-    exp_graph = graph_from_file(DATA_FOLDER POS_OUT("01"));
-    ck_assert_ptr_nonnull(exp_graph);
+#define TEST_DEBUG(name, num)                                                           \
+                        START_TEST( name )                                              \
+                        {                                                               \
+                            TEST_HEAD( num )                                            \
+                                                                                        \
+                            puts("graph");                                              \
+                            print_adjacency_list(graph);                                \
+                                                                                        \
+                            new_graph = remove_lambda_transitions(graph);               \
+                                                                                        \
+                            puts("exp_graph");                                          \
+                            print_adjacency_list(exp_graph);                            \
+                            puts("new_graph");                                          \
+                            print_adjacency_list(new_graph);                            \
+                                                                                        \
+                            graph_visualize(graph, "temp");                             \
+                            graph_visualize(new_graph, "temp_wo_lambda");               \
+                            graph_visualize(exp_graph, "temp_exp");                     \
+                                                                                        \
+                            TEST_TAIL                                                   \
+                        }                                                               \
+                        END_TEST
 
-    new_graph = remove_lambda_transitions(graph);
+TEST(test_rm_lambda_3_verts_2_lambdas, "01")
 
-    puts("exp_graph");
-    print_adjacency_list(exp_graph);
-    puts("new_graph");
-    print_adjacency_list(new_graph);
+TEST(test_rm_lambda_2_recursive_lambdas, "02")
 
-    graph_visualize(graph, "temp");
-    graph_visualize(new_graph, "temp_wo_lambda");
-    graph_visualize(exp_graph, "temp_exp");
+TEST(test_rm_lambda_3_recursive_lambdas, "03")
 
-    rc = compare_graphs(new_graph, exp_graph);
-    ck_assert_int_eq(rc, GRAPHS_EQUAL);
+TEST(test_rm_lambda_change_output, "04")
 
-    free_graph(graph);
-    free_graph(exp_graph);
-    free_graph(new_graph);
-}
-END_TEST
-
-START_TEST(test_rm_lambda_2_recursive_lambdas)
-{
-    int rc;
-
-    graph_t *graph;
-    graph_t *new_graph;
-    graph_t *exp_graph;
-
-    graph = graph_from_file(DATA_FOLDER POS_IN("02"));
-    ck_assert_ptr_nonnull(graph);
-
-    exp_graph = graph_from_file(DATA_FOLDER POS_OUT("02"));
-    ck_assert_ptr_nonnull(exp_graph);
-
-    new_graph = remove_lambda_transitions(graph);
-
-    rc = compare_graphs(new_graph, exp_graph);
-    ck_assert_int_eq(rc, GRAPHS_EQUAL);
-
-    free_graph(graph);
-    free_graph(exp_graph);
-    free_graph(new_graph);
-}
-END_TEST
-
-START_TEST(test_rm_lambda_3_recursive_lambdas)
-{
-    int rc;
-
-    graph_t *graph;
-    graph_t *new_graph;
-    graph_t *exp_graph;
-
-    graph = graph_from_file(DATA_FOLDER POS_IN("03"));
-    ck_assert_ptr_nonnull(graph);
-
-    exp_graph = graph_from_file(DATA_FOLDER POS_OUT("03"));
-    ck_assert_ptr_nonnull(exp_graph);
-
-    new_graph = remove_lambda_transitions(graph);
-
-    rc = compare_graphs(new_graph, exp_graph);
-    ck_assert_int_eq(rc, GRAPHS_EQUAL);
-
-    free_graph(graph);
-    free_graph(exp_graph);
-    free_graph(new_graph);
-}
-END_TEST
-
-START_TEST(test_rm_lambda_change_output)
-{
-    int rc;
-
-    graph_t *graph;
-    graph_t *new_graph;
-    graph_t *exp_graph;
-
-    graph = graph_from_file(DATA_FOLDER POS_IN("04"));
-    ck_assert_ptr_nonnull(graph);
-
-    exp_graph = graph_from_file(DATA_FOLDER POS_OUT("04"));
-    ck_assert_ptr_nonnull(exp_graph);
-
-    new_graph = remove_lambda_transitions(graph);
-
-    rc = compare_graphs(new_graph, exp_graph);
-    ck_assert_int_eq(rc, GRAPHS_EQUAL);
-
-    free_graph(graph);
-    free_graph(exp_graph);
-    free_graph(new_graph);
-}
-END_TEST
+TEST(test_rm_lambda_node_lambda_loop, "05")
 
 Suite *get_determinize_suite(void)
 {
@@ -129,6 +84,7 @@ Suite *get_determinize_suite(void)
     tcase_add_test(tc_pos, test_rm_lambda_2_recursive_lambdas);
     tcase_add_test(tc_pos, test_rm_lambda_3_recursive_lambdas);
     tcase_add_test(tc_pos, test_rm_lambda_change_output);
+    tcase_add_test(tc_pos, test_rm_lambda_node_lambda_loop);
     suite_add_tcase(s, tc_pos);
     return s;
 }
